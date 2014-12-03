ce0e581873c5c79ad3fb434f2d0a95f80d18754c
Connor Yamada <ckyamada@gmail.com>
2014-12-02 23:34:03 -0500

    update ip

--- fft.ino
+++ fft.ino
@@ -39,9 +39,15 @@ protected:
 
 MyWebSocket webSocketClient(client);
 
-//byte ip[] = { 192, 168, 2, 2 };
-char server[] = "169.254.73.27";  //websocket server
-char im[128], data[128];
+byte ip[] = { 10, 15, 0, 85 };
+byte gateway[] = { 10, 15, 0, 1 };
+byte subnet[] = { 255, 255, 255, 0 };
+//char server[] = "169.254.85.190";  //websocket server
+char server[] = "10.15.0.219";
+int sample_size = 2;
+int sample_const = 32;
+int sample = sample_size * sample_const;
+char im[64], data[64];
 int i = 0, val;
 //int peak = 0;
 
@@ -49,7 +55,7 @@ void setup() {
   Serial.begin(115200);
   
   uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x73, 0xBF }; //mac address of the ethernet board
-  Ethernet.begin(mac);
+  Ethernet.begin(mac, ip, gateway, subnet);
   
   if (client.connect(server, 8080)) {
     Serial.println("Connected");
@@ -65,41 +71,75 @@ void setup() {
     Serial.println("Handshake failed.");  
     hang();// Hang on failure
   }
-  webSocketClient.sendData("hello world!");  //init
   client.flush();
+  
 }
 
 void loop() {
-  String dataStr;
-  for (i=0; i < 128; i++) {                                     
-    val = analogRead(0);        //electret microphone attached to A0                            
-    data[i] = val/4 -128;                                       
-    im[i] = 0;                                                      
-  }
-
-  fix_fft(data,im,7,0);
-
-  for (i=1; i< 64;i++) {       //values from 0-500Hz are trash      
-    data[i] = sqrt(data[i] * data[i] + im[i] * im[i]);  
-    /*
-    if (data[i] > peak) {
-      peak = data[i] + 1;
+  String dataStr = "{\"channel\":\"in\",\"data\":\"";
+  //String dataStr;
+  if (client.connected()) {
+    
+    for (i=0; i < sample; i++) {                                     
+      val = analogRead(0);        //electret microphone attached to A0                            
+      data[i] = val/4 -sample  ;                                       
+      im[i] = 0;                                                      
     }
-    */
-    String temp = String((data[i] + 0)); 
-    dataStr += temp + " ";
-  }
   
-  Serial.println(dataStr);        
-  char arr[126];                   //126 char max
-  dataStr.toCharArray(arr, 126);
-  webSocketClient.sendData(arr);
-  //String peakStr = String(peak);
-  //Serial.println(peak);
-  //Serial.println(dataStr);
-  //itoa(peak, dat, 6);
-  //peak = 0;
-  client.flush();
-  delay(1);
+    fix_fft(data,im,4 + sample_size,0);
+  
+    for (i=1; i< (sample / 2);i++) {       //values from 0-500Hz are trash      
+      data[i] = sqrt(data[i] * data[i] + im[i] * im[i]);  
+      /*
+      if (data[i] > peak) {
+        peak = data[i] + 1;
+      }
+      */
+      
+        String temp = String((data[i] + 0)); 
+        dataStr += temp + " ";
+     
+    }
+    dataStr += "\"}";
+    
+    Serial.println(dataStr);  
+      
+    char arr[126];                   //126 char max
+    dataStr.toCharArray(arr, 126);
+    webSocketClient.sendData(arr);
+    //String peakStr = String(peak);
+    //Serial.println(peak);
+    //Serial.println(dataStr);
+    //itoa(peak, dat, 6);
+    //peak = 0;
+    client.flush();
+    delay(1);
+  } else {
+    Serial.println("The connection has been interrupted");
+    client.flush();
+    delay(10000);
+    reconnect();
+  }
 }  
+ 
+void reconnect() {
+  uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x73, 0xBF }; //mac address of the ethernet board
+  Ethernet.begin(mac, ip, gateway, subnet);
+  
+  if (client.connect(server, 8080)) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Connection failed.");
+    hang();// Hang on failure
+  }
+  Serial.println("end setup");
+  
+  if (websocket::clientHandshake(client, server, "/") == websocket::Success_Ok) {  
+    Serial.println("Handshake successful");
+  } else {
+    Serial.println("Handshake failed.");  
+    hang();// Hang on failure
+  }
+  client.flush();
+}
   
